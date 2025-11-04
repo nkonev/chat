b83fd3eb0e75387100cffbec7e3313aeba4c33e7
Nikita Konev <nikit.cpp@yandex.ru>
2025-11-04 03:05:56 +0300

    sep ctx

--- cqrs/command_handler.go
+++ cqrs/command_handler.go
@@ -577,15 +577,16 @@ func (s *ParticipantDelete) Handle(ctx context.Context, eventBus EventBusInterfa
 	return nil
 }
 
-func (s *Truncate) Handle(ctx context.Context, eventBus EventBusInterface, dba *db.DB, commonProjection *CommonProjection, lgr *logger.LoggerWrapper, cfg *config.AppConfig) error {
+func (s *Truncate) Handle(ctx0 context.Context, eventBus EventBusInterface, dba *db.DB, commonProjection *CommonProjection, lgr *logger.LoggerWrapper, cfg *config.AppConfig) error {
 	pa := &ProjectionsTruncated{}
-	err := eventBus.Publish(ctx, pa)
+	err := eventBus.Publish(ctx0, pa)
 	if err != nil {
 		return err
 	}
 
 	i := 0
 	for {
+		ctx := context.Background()
 		completed, err := commonProjection.GetIsTruncatingCompleted(ctx, dba)
 		if err != nil {
 			lgr.InfoContext(ctx, "error during GetIsTruncatingCompleted", "err", err)
