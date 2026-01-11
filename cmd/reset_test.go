1f2b5b20e015995c3a1cbb9d093582e613dd852f
Nikita Konev <nikit.cpp@yandex.ru>
2026-01-11 17:43:55 +0300

    fix reset test

--- cmd/reset_test.go
+++ cmd/reset_test.go
@@ -64,7 +64,7 @@ func TestReset(t *testing.T) {
 		lc fx.Lifecycle,
 	) {
 		mockAaaClient := aaaRestClient.(*client.MockAaaRestClient)
-		mockAaaClient.EXPECT().GetUsers(mock.Anything, []int64{user1}).Return([]*dto.User{&mockUser1}, nil)
+		mockAaaClient.EXPECT().GetUsers(mock.Anything, mock.Anything).Return([]*dto.User{&mockUser1}, nil)
 
 		ctx := context.Background()
 
@@ -145,7 +145,7 @@ func TestReset(t *testing.T) {
 		lc fx.Lifecycle,
 	) {
 		mockAaaClient := aaaRestClient.(*client.MockAaaRestClient)
-		mockAaaClient.EXPECT().GetUsers(mock.Anything, []int64{user1}).Return([]*dto.User{&mockUser1}, nil)
+		mockAaaClient.EXPECT().GetUsers(mock.Anything, mock.Anything).Return([]*dto.User{&mockUser1}, nil)
 
 		ctx := context.Background()
 
