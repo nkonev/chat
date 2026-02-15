8739bca14fc51cedf99e1026c4c74b28fc18a350
Nikita Konev <nikit.cpp@yandex.ru>
2026-02-15 04:13:13 +0300

    add awaiting for postgresql

--- scripts/wait-for-postgresql.sh
+++ scripts/wait-for-postgresql.sh
@@ -8,32 +8,26 @@ echo "Waiting for $PG_USER"
 
 set +e
 
+function check_string() {
+      echo -n "$1" | grep "$2" &> /dev/null
+      if [[ $? == 0 ]]; then
+        return
+      fi
+
+      false
+}
+
 for ((n=0; n<=$MAX_ATTEMPTS; n++))
 do
   echo "attempt $n / $MAX_ATTEMPTS"
 
   output=`docker compose exec postgresql-citus-coordinator-1 psql -U ${PG_USER} -d ${DB_NAME} --csv --tuples-only -c 'SELECT nodename FROM citus_nodes;'`
 
-  has_coordinator_1=false
-  has_worker_1=false
-  has_worker_2=false
+  if \
+  check_string "$output" 'postgresql-citus-coordinator-1' && \
+  check_string "$output" 'postgresql-citus-worker-1' && \
+  check_string "$output" 'postgresql-citus-worker-2'; then
 
-  echo -n $output | grep 'postgresql-citus-coordinator-1' &> /dev/null
-  if [[ $? == 0 ]]; then
-    has_coordinator_1=true
-  fi
-
-  echo -n $output | grep 'postgresql-citus-worker-1' &> /dev/null
-  if [[ $? == 0 ]]; then
-    has_worker_1=true
-  fi
-
-  echo -n $output | grep 'postgresql-citus-worker-2' &> /dev/null
-  if [[ $? == 0 ]]; then
-    has_worker_2=true
-  fi
-
-  if [[ "$has_coordinator_1" = true && "$has_worker_1" = true && "$has_worker_2" = true ]]; then
     echo "PostgreSQL is available"
     exit 0
   fi
