5a77c2e4bf1b03733c45b4f21eae9674663ff232
Nikita Konev <nikit.cpp@yandex.ru>
2026-02-15 03:20:09 +0300

    optimize PostgreSQL

--- docker/postgresql-citus/common/docker-entrypoint-initdb.d/002-common-init.sql
+++ docker/postgresql-citus/common/docker-entrypoint-initdb.d/002-common-init.sql
@@ -1,17 +1,17 @@
 -- ALTER SYSTEM SET max_connections = 400;
 -- Uncomment if you need to view the full postgres logs (SQL statements, ...) via `docker logs -f postgresql-test`
-ALTER SYSTEM SET log_statement = 'mod';
+--ALTER SYSTEM SET log_statement = 'mod';
 ALTER SYSTEM SET synchronous_commit = 'off'; -- https://postgrespro.ru/docs/postgrespro/9.5/runtime-config-wal.html#GUC-SYNCHRONOUS-COMMIT
--- ALTER SYSTEM SET shared_buffers='512MB';
+ALTER SYSTEM SET shared_buffers='512MB';
 ALTER SYSTEM SET fsync=FALSE;
 ALTER SYSTEM SET full_page_writes=FALSE;
-ALTER SYSTEM SET commit_delay=100000;
-ALTER SYSTEM SET commit_siblings=10;
+-- ALTER SYSTEM SET commit_delay=100000;
+-- ALTER SYSTEM SET commit_siblings=10;
 ALTER SYSTEM SET log_line_prefix = '%a %u@%d ';
 
 -- https://docs.citusdata.com/en/v11.2/performance/performance_tuning.html
 alter system set citus.max_adaptive_executor_pool_size = 1;
-alter system set work_mem = '256MB';
+alter system set work_mem = '512MB';
 alter system set citus.executor_slow_start_interval = 200;
 
 -- https://docs.citusdata.com/en/stable/admin_guide/cluster_management.html#create-db
