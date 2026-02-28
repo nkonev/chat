2c4f6f95fe14cef7d1dffa77801414c3312bf5d9
Nikita Konev <nikit.cpp@yandex.ru>
2026-02-28 22:03:20 +0300

    print offset

--- cqrs/cqrs.go
+++ cqrs/cqrs.go
@@ -315,9 +315,9 @@ func processEvent[T any](lgr *logger.LoggerWrapper, cfg *config.AppConfig, event
 
 	if cfg.Cqrs.Dump {
 		if cfg.Cqrs.PrettyLog && !cfg.Logger.Json {
-			fmt.Printf("[kafka cqrs subscriber] Processing record: trace_id=%s, topic=%s, event_type=%v, body: %v\n", logger.GetTraceId(ctx), record.Topic, eventType, string(record.Value))
+			fmt.Printf("[kafka cqrs subscriber] Processing record: trace_id=%s, topic=%s, offset=%d, event_type=%v, body: %v\n", logger.GetTraceId(ctx), record.Topic, record.Offset, eventType, string(record.Value))
 		} else {
-			lgr.InfoContext(ctx, "[kafka cqrs subscriber] Processing record:", "topic", record.Topic, "partition", record.Partition, "event_type", eventType, "key", string(record.Key), "value", string(record.Value))
+			lgr.InfoContext(ctx, "[kafka cqrs subscriber] Processing record:", "topic", record.Topic, "offset", record.Offset, "partition", record.Partition, "event_type", eventType, "key", string(record.Key), "value", string(record.Value))
 		}
 	}
 
